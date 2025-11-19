acdb6fc565d1fdfaa1b8e1e4abbec24dccf396dc
Abel-Mitiku <abelmitiku461@gmail.com>
2025-11-19 16:04:03 +0300

    Final

--- app/modelY/components/teslalogo.js
+++ app/modelY/components/teslalogo.js
@@ -6,7 +6,7 @@ export default function TeslaLogo() {
   return (
     <div className="relative">
       <div className="z-30 tesla-logo">
-        <div className="text-black dark:black flex justify-between [@media(min-width:500px)]:mt-4 w-screen bg-white pt-4">
+        <div className="text-black dark:black flex justify-between [@media(min-width:500px)]:mt-4 w-screen bg-white pt-4 h-12">
           <a href="#" className="ml-4 [@media(min-width:500px)]:ml-8">
             T E S L A
           </a>
